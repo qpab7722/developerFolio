69775589013fb81a13f307cd2d89051cd17d4d4b
YJ <qpab7722@gmail.com>
2021-01-26 11:04:15 +0900

    Updates

---
+++ precache-manifest.6cf830bbd56a531a54484c4e89133756.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffcf58091fb154580a7ef916fa468fe",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "b37e567bab47f4a57b81",
+    "url": "/developerFolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "3c5e4e4e8e54ab1ff814",
+    "url": "/developerFolio/static/css/4.21259c32.chunk.css"
+  },
+  {
+    "revision": "8c1b5f820efb617aeea9",
+    "url": "/developerFolio/static/css/main.d75a6cb4.chunk.css"
+  },
+  {
+    "revision": "755887161686312fc3fd",
+    "url": "/developerFolio/static/js/2.10553b47.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/developerFolio/static/js/2.10553b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37e567bab47f4a57b81",
+    "url": "/developerFolio/static/js/3.2a082d9b.chunk.js"
+  },
+  {
+    "revision": "3c5e4e4e8e54ab1ff814",
+    "url": "/developerFolio/static/js/4.0ec8e3d7.chunk.js"
+  },
+  {
+    "revision": "8c1b5f820efb617aeea9",
+    "url": "/developerFolio/static/js/main.4c763b6b.chunk.js"
+  },
+  {
+    "revision": "823bb7d0a7701e550163",
+    "url": "/developerFolio/static/js/runtime-main.cbe3fd2a.js"
+  },
+  {
+    "revision": "edf6632cd7714681ac9a3033c06a02c7",
+    "url": "/developerFolio/static/media/911.edf6632c.PNG"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "c2d65730d1f89e06437cbef712d750ab",
+    "url": "/developerFolio/static/media/hack.c2d65730.JPG"
+  },
+  {
+    "revision": "c1c59da5e8d178252d614054d45e277f",
+    "url": "/developerFolio/static/media/heart.c1c59da5.PNG"
+  },
+  {
+    "revision": "6f5c5053787c6851397168618eac061a",
+    "url": "/developerFolio/static/media/kcgs.6f5c5053.PNG"
+  },
+  {
+    "revision": "3c5151c1a38ed9946626a609b2fd5907",
+    "url": "/developerFolio/static/media/pacman.3c5151c1.gif"
+  },
+  {
+    "revision": "54a55c504637a0d3f8cfd7152795d085",
+    "url": "/developerFolio/static/media/sejong.54a55c50.PNG"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f090cb5857741fcf42a2c90567c21430",
+    "url": "/developerFolio/static/media/sw4.f090cb58.JPG"
+  },
+  {
+    "revision": "e3c6fbf70b5cc0ecbc06e0dee2346a89",
+    "url": "/developerFolio/static/media/swlockermanager.e3c6fbf7.PNG"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/developerFolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "0b117a5c3c4c3e317db6bbc646597195",
+    "url": "/developerFolio/static/media/yeji.0b117a5c.PNG"
+  }
+]);
